b03890865c22e288e68c6ab97bfb318feffe5fba
Tom Pruiksma <tom.pruiksma@gmail.com>
2020-12-06 12:42:42 +0200

    Day 6

---
+++ 2020/Day6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var groups = new List<List<string>>();
+            var group = new List<string>();
+            foreach(var line in input) {
+                if (string.IsNullOrWhiteSpace(line)) {
+                    groups.Add(group);
+                    group = new List<string>();
+                    continue;
+                }
+                group.Add(line);
+            }
+            groups.Add(group);
+
+            Console.WriteLine("Solving puzzle 1...");
+            var sum = groups.Sum(g => g.Aggregate("", (a, b) => $"{a}{b}").Distinct().Count());
+            Console.WriteLine($"Requested sum is {sum}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Solving puzzle 2...");
+            var sum2 = 0;
+            foreach(var g in groups) {
+                var groupsSum = 0;
+                foreach(var c in g.First()) {
+                    if (g.All(current => current.Contains(c))) groupsSum++;
+                }
+                sum2 += groupsSum;
+            }
+            Console.WriteLine($"Requested sum is {sum2}");
+        }
+    }
+}

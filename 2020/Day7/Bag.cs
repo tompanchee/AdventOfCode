e9beed8917ec1081dd9adc41ba4dd38739b0fc4f
Pruiksma, Tom <Tom.Pruiksma@cdk.com>
2020-12-07 11:37:42 +0200

    Day 7

---
+++ 2020/Day7/Bag.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day7
+{
+    class Bag
+    {
+        public static Bag Parse(string input) {
+            var outerEnd = input.IndexOf("bags contain", StringComparison.InvariantCulture);
+            var outer = input.Substring(0, outerEnd).Trim();
+
+            var bag = new Bag(outer);
+
+            if (!input.Contains("no other bags")) {
+                var inners = input.Substring(outerEnd + "bags contain".Length).Trim().Split(',', StringSplitOptions.TrimEntries);
+                foreach (var inner in inners) {
+                    var amount = int.Parse(inner.Substring(0, 1));
+                    var color = inner.Substring(1)
+                        .Replace("bags.", "")
+                        .Replace("bags", "")
+                        .Replace("bag.", "")
+                        .Replace("bag", "")
+                        .Trim();
+                    bag.InnerBags.Add(new InnerBag {
+                        Bag = new Bag(color),
+                        Amount = amount
+                    });
+                }
+            }
+
+            return bag;
+        }
+
+        private Bag(string color) {
+            Color = color;
+        }
+
+        public string Color { get; }
+
+        public List<InnerBag> InnerBags { get; } = new List<InnerBag>();
+
+        public bool HasChildWithColor(string color) {
+            if (InnerBags.Count == 0) return false;
+            if (InnerBags.Any(b => b.Bag.Color == color)) return true;
+
+            return InnerBags.Aggregate(false, (current, innerBag) => current || innerBag.Bag.HasChildWithColor(color));
+        }
+
+        public int TotalInnerBagCount
+        {
+            get
+            {
+                if (InnerBags.Count == 0) return 1;
+                var sum = InnerBags.Sum(innerBag => innerBag.Amount * innerBag.Bag.TotalInnerBagCount);
+
+                return sum + 1; // Add self
+            }
+        }
+
+        public class InnerBag
+        {
+            public Bag Bag { get; set; }
+            public int Amount { get; set; }
+        }
+    }
+}
---
+++ 2020/Day7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var input = File.ReadAllLines("input.txt");
+            var bags = (from line in input where !string.IsNullOrEmpty(line) select Bag.Parse(line)).ToList();
+            BuildBagTrees(bags);
+
+            Solve1(bags);
+            Console.WriteLine();
+            Solve2(bags);
+        }
+
+        private static void Solve1(IEnumerable<Bag> bags) {
+            Console.WriteLine("Solving puzzle 1...");
+            var count = bags.Count(bag => bag.HasChildWithColor("shiny gold"));
+
+            Console.WriteLine($"There are {count} bags that can carry at least one shiny gold bag");
+        }
+
+        private static void Solve2(IEnumerable<Bag> bags) {
+            Console.WriteLine("Solving puzzle 2...");
+            var shinyGold = bags.Single(b => b.Color == "shiny gold");
+            Console.WriteLine($"Shiny gold bag contains {shinyGold.TotalInnerBagCount - 1} bags"); // Remove self
+        }
+
+        private static void BuildBagTrees(IReadOnlyCollection<Bag> bags) {
+            foreach (var bag in bags) {
+                foreach (var innerBag in bag.InnerBags) {
+                    var inner = bags.Single(b => b.Color == innerBag.Bag.Color);
+                    innerBag.Bag = inner;
+                }
+            }
+        }
+    }
+}

e0e05f0b47552557beacb20214f15417ef9e45db
Pruiksma, Tom <Tom.Pruiksma@cdk.com>
2020-12-08 08:32:46 +0200

    Day 8

---
+++ 2020/Day8/GameConsole.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day8
+{
+    class GameConsole
+    {
+        private readonly string[] program;
+        private int pc;
+
+        public GameConsole(string[] program) {
+            this.program = program;
+        }
+
+        public int Execute(bool haltOnLoop = false) {
+            pc = -1;
+            var executedInstructions = new List<int>();
+
+            while (true) {
+                pc++;
+                if (haltOnLoop && executedInstructions.Contains(pc)) {
+                    Console.WriteLine("Loop found halted");
+                    return -1;
+                }
+
+                executedInstructions.Add(pc);
+
+                var (opCode, argument) = ParseInstruction(program[pc]);
+
+                switch (opCode) {
+                    case "acc":
+                        Acc += int.Parse(argument);
+                        break;
+                    case "jmp":
+                        pc += int.Parse(argument) - 1; // subtract one as pc is any way increased in the loop
+                        break;
+                    case "nop":
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Unknown operation code {opCode}");
+                }
+
+                if (pc == program.Length - 1) {
+                    Console.WriteLine("Executed last instruction");
+                    return 0;
+                }
+            }
+        }
+
+        private static (string, string) ParseInstruction(string instruction) {
+            var split = instruction.Split(' ');
+            return (split[0], split[1]);
+        }
+
+        public int Acc { get; private set; }
+    }
+}
---
+++ 2020/Day8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var input = File.ReadAllLines("input.txt").Where(i => !string.IsNullOrWhiteSpace(i)).ToArray();
+
+            Solve1(input);
+            Console.WriteLine();
+            Solve2(input);
+        }
+
+        private static void Solve2(string[] input) {
+            Console.WriteLine("Solving puzzle 1...");
+            for (var i = 0; i < input.Length; i++) {
+                var @fixed = (string[]) input.Clone();
+                if (@fixed[i].StartsWith("acc")) continue;
+                if (@fixed[i].StartsWith("nop")) @fixed[i] = @fixed[i].Replace("nop", "jmp");
+                else @fixed[i] = @fixed[i].Replace("jmp", "nop");
+
+                var console = new GameConsole(@fixed);
+                if (console.Execute(true) == 0) {
+                    Console.WriteLine($"Acc after fixed program is executed is {console.Acc}");
+                    break;
+                }
+            }
+        }
+
+        private static void Solve1(string[] input) {
+            Console.WriteLine("Solving puzzle 1...");
+            var console = new GameConsole(input);
+            console.Execute(true);
+            Console.WriteLine($"Acc value at loop is {console.Acc}");
+        }
+    }
+}

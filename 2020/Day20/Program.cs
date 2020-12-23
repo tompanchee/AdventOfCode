65363143c7c8a8f181d17582d170e03fc0baef2e
Pruiksma, Tom <Tom.Pruiksma@cdk.com>
2020-12-23 08:28:40 +0200

    Day 20 - part 1

---
+++ 2020/Day20/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day20
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var input = File.ReadAllLines("input.txt");
+            var tiles = ParseInput(input);
+            CalculatePossibleNeighbours(tiles);
+
+            Console.WriteLine("Solving puzzle 1...");
+            var corners = tiles.Where(t => t.PossibleNeighbours.Count == 2);
+            var product = corners.Aggregate(1L, (current, tile) => current * tile.Id);
+            Console.WriteLine($"Product of corner tile ids is {product}");
+        }
+
+        private static void CalculatePossibleNeighbours(Tile[] tiles) {
+            foreach (var tile in tiles) {
+                foreach (var edge in tile.Edges) {
+                    var candidates = tiles.Where(t => t.Id != tile.Id && t.Edges.Contains(edge));
+                    foreach (var candidate in candidates) {
+                        if (!tile.PossibleNeighbours.Contains(candidate)) tile.PossibleNeighbours.Add(candidate);
+                    }
+                }
+            }
+        }
+
+        private static Tile[] ParseInput(string[] input) {
+            var tiles = new List<Tile>();
+
+            var tileLines = new List<string>();
+            foreach (var line in input) {
+                if (string.IsNullOrWhiteSpace(line)) {
+                    AddTile();
+                    tileLines = new List<string>();
+                    continue;
+                }
+
+                tileLines.Add(line);
+            }
+
+            return tiles.ToArray();
+
+            void AddTile() {
+                if (tileLines.Count < 2) return;
+                var id = int.Parse(tileLines[0][5..^1]);
+                tiles.Add(new Tile(id, tileLines.Skip(1).ToArray()));
+            }
+        }
+    }
+}
---
+++ 2020/Day20/Tile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day20
+{
+    class Tile
+    {
+        private readonly string[] rows;
+
+        public Tile(int id, string[] rows) {
+            Id = id;
+            this.rows = rows;
+
+            CalculateEdges();
+        }
+
+        public int Id { get; }
+
+        public string[] Edges { get; private set; }
+
+        private void CalculateEdges() {
+            var edges = new List<string> {
+                // Top
+                rows[0],
+                // Right
+                new string(rows.Select(r => r.Last()).ToArray()),
+                // Bottom
+                rows.Last(),
+                // Left
+                new string(rows.Select(r => r[0]).Reverse().ToArray())
+            };
+
+            // Flipped
+
+            // Top
+            edges.Add(Reverse(edges[0]));
+            // Right
+            edges.Add(Reverse(edges[1]));
+            // Bottom
+            edges.Add(Reverse(edges[2]));
+            // Left
+            edges.Add(Reverse(edges[3]));
+
+            Edges = edges.ToArray();
+        }
+
+        static string Reverse(string s) {
+            return new string(s.Reverse().ToArray());
+        }
+
+        public List<Tile> PossibleNeighbours { get; } = new List<Tile>();
+    }
+}

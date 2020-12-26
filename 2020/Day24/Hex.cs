7fe5a697c90f2ef195878c87a11a29bdb5206c62
Tom Pruiksma <tom.pruiksma@gmail.com>
2020-12-26 10:41:38 +0200

    Day 24

---
+++ 2020/Day24/Hex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day24
+{
+    class Hex
+    {
+        private static readonly Dictionary<string, (int, int)> moves = new() {
+            {"ne", (1, -1)},
+            {"e", (2, 0)},
+            {"se", (1, 1)},
+            {"sw", (-1, 1)},
+            {"w", (-2, 0)},
+            {"nw", (-1, -1)},
+        };
+
+        private readonly int x;
+        private readonly int y;
+
+        public Hex() : this(0, 0) { }
+
+        public Hex(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Hex((int, int) coordinates) : this(coordinates.Item1, coordinates.Item2) { }
+
+        public (int x, int y) Coordinates => (x, y);
+
+        public Hex Move(string move) {
+            var m = move.ToLowerInvariant();
+            if (!moves.ContainsKey(m)) throw new InvalidOperationException($"Unknown move {move}");
+            return new Hex(x + moves[m].Item1, y + moves[m].Item2);
+        }
+
+        public IEnumerable<(int, int)> GetNeighbourCoordinates() => moves.Select(d => (x + d.Value.Item1, y + d.Value.Item2)).ToList();
+    }
+}
---
+++ 2020/Day24/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day24
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var input = File.ReadAllLines("input.txt");
+
+            Console.WriteLine("Solving puzzle 1...");
+            var blackTiles = new HashSet<(int, int)>();
+            foreach (var line in input) {
+                var tile = new Hex();
+                tile = ParseMoves(line).Aggregate(tile, (current, move) => current.Move(move));
+                if (blackTiles.Contains(tile.Coordinates)) blackTiles.Remove(tile.Coordinates);
+                else blackTiles.Add(tile.Coordinates);
+            }
+
+            Console.WriteLine($"Finally there are {blackTiles.Count} black tiles");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Solving puzzle 2...");
+            for (var i = 0; i < 100; i++) {
+                var minX = blackTiles.Min(h => h.Item1) - 4;
+                var maxX = blackTiles.Max(h => h.Item1) + 4;
+                var minY = blackTiles.Min(h => h.Item2) - 2;
+                var maxY = blackTiles.Max(h => h.Item2) + 2;
+
+                var nextGen = new HashSet<(int, int)>();
+                for (var x = minX; x < maxX; x++) {
+                    for (var y = minY; y < maxY; y++) {
+                        if ((x + y) % 2 != 0) continue;
+                        var hex = new Hex(x, y);
+                        var blackNeighbourCount = hex.GetNeighbourCoordinates().Count(neighbour => blackTiles.Contains(new Hex(neighbour).Coordinates));
+                        if (blackTiles.Contains(hex.Coordinates)) {
+                            if (blackNeighbourCount > 0 && blackNeighbourCount <= 2) nextGen.Add(hex.Coordinates);
+                        } else {
+                            if (blackNeighbourCount == 2) nextGen.Add(hex.Coordinates);
+                        }
+                    }
+                }
+
+                blackTiles = nextGen;
+            }
+
+            Console.WriteLine($"Finally there are {blackTiles.Count} black tiles");
+        }
+
+        private static IEnumerable<string> ParseMoves(string line) {
+            var moves = new List<string>();
+            for (var i = 0; i < line.Length; i++) {
+                if (line[i] == 'e' || line[i] == 'w') {
+                    moves.Add(line.Substring(i, 1));
+                    continue;
+                }
+
+                moves.Add(line.Substring(i, 2));
+                i += 1;
+            }
+
+            return moves;
+        }
+    }
+}

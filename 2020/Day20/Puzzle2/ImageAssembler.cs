9217ddbc4f164f755a0a4b85b7225435f50fd06f
Pruiksma, Tom <Tom.Pruiksma@cdk.com>
2021-01-07 15:45:00 +0200

    Day 20 complete

---
+++ 2020/Day20/Puzzle2/ImageAssembler.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day20.Puzzle2
+{
+    static class ImageAssembler
+    {
+        public static Tile AssembleImage(Tile[] tiles) {
+            var finalSize = (int) Math.Sqrt(tiles.Length); // Square image
+
+            var assembled = new Tile[finalSize, finalSize];
+
+            for (var row = 0; row < finalSize; row++) {
+                for (var col = 0; col < finalSize; col++) {
+                    switch (row) {
+                        case 0 when col == 0:
+                            assembled[0, 0] = SetCorner(tiles);
+                            continue;
+                        case 0:
+                            assembled[0, col] = SetRightNeighbour(tiles, assembled[0, col - 1]);
+                            continue;
+                        default:
+                            assembled[row, col] = SetBottomNeighbour(tiles, assembled[row - 1, col]);
+                            break;
+                    }
+                }
+            }
+
+            for (var row = 0; row < finalSize; row++) {
+                for (var col = 0; col < finalSize; col++) {
+                    assembled[row, col] = assembled[row, col].RemoveEdges();
+                }
+            }
+
+            return Combine(assembled);
+        }
+
+        private static Tile Combine(Tile[,] assembled) {
+            var size = assembled[0, 0].Size;
+            var rows = new List<string>();
+            for (var row = 0; row < assembled.GetLength(0); row++) {
+                var r2 = new List<string>();
+                for (var i = 0; i < size; i++) {
+                    var newRow = "";
+                    for (var col = 0; col < assembled.GetLength(1); col++) {
+                        newRow += assembled[row, col].Rows[i];
+                    }
+
+                    r2.Add(newRow);
+                }
+
+                rows.AddRange(r2);
+            }
+
+            return new Tile(0, rows.ToArray());
+        }
+
+        private static Tile SetCorner(Tile[] tiles) {
+            var corner = tiles.First(t => t.GetNeighbours(tiles).Count == 2);
+            while (corner.FindNeighbourWithEdge(corner.Right, tiles) == null || corner.FindNeighbourWithEdge(corner.Bottom, tiles) == null) {
+                corner.Rotate();
+            }
+
+            return corner;
+        }
+
+        private static Tile SetRightNeighbour(Tile[] tiles, Tile tile) {
+            var right = tile.FindNeighbourWithEdge(tile.Right, tiles);
+            for (var i = 0; i < 4; i++) {
+                if (IsMatch()) return right;
+                // Check flipped
+                right.FlipHorizontally();
+                if (IsMatch()) return right;
+                // Flip back
+                right.FlipHorizontally();
+                // ... and rotate
+                right.Rotate();
+            }
+
+            throw new Exception("Invalid tile");
+
+            bool IsMatch() {
+                return tile.Right == right.Left;
+            }
+        }
+
+        private static Tile SetBottomNeighbour(Tile[] tiles, Tile tile) {
+            var bottom = tile.FindNeighbourWithEdge(tile.Bottom, tiles);
+            for (var i = 0; i < 4; i++) {
+                if (IsMatch()) return bottom;
+                // Check flipped
+                bottom.FlipVertically();
+                if (IsMatch()) return bottom;
+                // Flip back
+                bottom.FlipVertically();
+                // ... and rotate
+                bottom.Rotate();
+            }
+
+            throw new Exception("Invalid tile");
+
+            bool IsMatch() {
+                return tile.Bottom == bottom.Top;
+            }
+        }
+    }
+}
---
+++ 2020/Day20/Puzzle2/Solver.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day20.Puzzle2
+{
+    class Solver
+    {
+        static readonly string[] monster = {
+            "                  # ",
+            "#    ##    ##    ###",
+            " #  #  #  #  #  #   "
+        };
+
+        public static void Solve(Tile[] tiles) {
+            Console.WriteLine("Solving puzzle 2...");
+            var water = ImageAssembler.AssembleImage(tiles);
+            var monsterCount = CalculateMonsters(water);
+
+            var monsterPoints = monster.Aggregate(0, (a, r) => a + r.Count(c => c == '#'));
+            var points = water.Rows.Aggregate(0, (a, r) => a + r.Count(c => c == '#'));
+
+            Console.WriteLine($"Total roughness is {points - monsterCount * monsterPoints}");
+        }
+
+        static int CalculateMonsters(Tile water) {
+            var monsterCoords = GetMonsterCoords();
+            var height = monster.Length;
+            var width = monster[0].Length;
+            var count = 0;
+            for (var i = 0; i < 4; i++) {
+                var monsterCount = 0;
+                for (var row = 0; row < water.Size - height; row++) {
+                    for (var col = 0; col < water.Size - width; col++) {
+                        var subArray = water.GetSubArray((row, col), (height, width));
+                        if (HasMonster(subArray, monsterCoords)) monsterCount++;
+                    }
+                }
+
+                if (monsterCount > count) count = monsterCount;
+                water.Rotate();
+            }
+
+            return count;
+        }
+
+        private static bool HasMonster(IReadOnlyList<string> subArray, IEnumerable<(int, int )> monsterCoords) {
+            foreach (var (row, col) in monsterCoords) {
+                if (subArray[row][col] != '#') return false;
+            }
+
+            return true;
+        }
+
+        private static (int, int)[] GetMonsterCoords() {
+            var coords = new List<(int, int)>();
+            for (var row = 0; row < monster.Length; row++) {
+                for (var col = 0; col < monster[row].Length; col++) {
+                    if (monster[row][col] == '#') coords.Add((row, col));
+                }
+            }
+
+            return coords.ToArray();
+        }
+    }
+}

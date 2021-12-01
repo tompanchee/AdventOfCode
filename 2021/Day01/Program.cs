ca2ee21847b956feadc01b46a2ec6be759af7b36
Pruiksma, Tom <tom.pruiksma@keyloop.com>
2021-12-01 09:14:15 +0200

    2021 - Day 1

---
+++ 2021/Day01/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = File.ReadAllLines("input.txt").Select(l=>int.Parse(l)).ToArray();
+
+Console.WriteLine("Solving puzzle 1...");
+
+var count = 0;
+for(var i=1; i < input.Length; i++) {
+    if (input[i] > input[i-1]) count++;
+}
+Console.WriteLine($"Depth increases {count} times");
+
+Console.WriteLine();
+
+Console.WriteLine("Solving puzzle 2...");
+count = 0;
+for(var i=0; i < input.Length-3; i++) {
+    var current = input.Skip(i).Take(3).Sum();
+    var next = input.Skip(i+1).Take(3).Sum();
+
+    if (next > current) count++;
+}
+Console.WriteLine($"Depth increases {count} times");

ae637736ee68301e7c037f3a8ee520439c98e4a7
Tom Pruiksma <tom.pruiksma@gmail.com>
2021-12-04 09:39:54 +0200

    Day 4

---
+++ 2021/Day04/Board.cs
@@ -0,0 +1,71 @@
+﻿namespace Day04
+{
+    internal class Board
+    {
+        const int TABLE_SIZE = 5;
+        readonly int[,] table = new int[TABLE_SIZE, TABLE_SIZE];
+
+        private Board(int[,] table, int id) {
+            this.table = table;
+            Id = id;
+        }
+
+        public static Board From(string[] data, int id) {
+            var table = new int[TABLE_SIZE, TABLE_SIZE];
+            for (var row = 0; row < data.Length; row++) {
+                var rowData = data[row].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for(var col = 0; col < rowData.Length; col++) {
+                    table[row, col] = rowData[col];
+                }
+            }
+
+            return new Board(table, id);
+        }
+
+        public void DrawNumber(int number) {
+            for(var row = 0; row < TABLE_SIZE; row++) {
+                for (var col = 0; col < TABLE_SIZE; col++) {
+                    if(table[row, col] == number) { 
+                        table[row, col] = -1; // Mark found number as -1
+                    }
+                }
+            }
+        }
+
+        public int GetWinningScore(int number) {
+            var sum = table.Cast<int>().Where(n => n > 0).Sum();
+            return number * sum;
+        }
+
+        public bool IsBingo() {
+            for(var i=0; i<TABLE_SIZE; i++) {
+                var row = Row(i);
+                var col = Col(i);
+
+                if (row.All(n => n < 0) || col.All(n => n < 0)) return true;
+            }
+
+            return false;
+        }
+
+        public int Id { get; }
+
+        int[] Row(int row) {
+            var result = new List<int>();
+            for (var i = 0; i < TABLE_SIZE; i++) {
+                result.Add(table[row, i]);
+            }
+
+            return result.ToArray();
+        }
+
+        int[] Col(int col) {
+            var result = new List<int>();
+            for(var i=0; i < TABLE_SIZE; i++) {
+                result.Add(table[i, col]);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ 2021/Day04/Program.cs
@@ -0,0 +1,60 @@
+﻿using Day04;
+
+var input = File.ReadAllLines("input.txt");
+var (numbers, boards) = ParseInput(input);
+
+Console.WriteLine("Solving puzzle 1...");
+var (number, winner) = Play(boards, numbers);
+Console.WriteLine($"Winning board score is {winner.GetWinningScore(number)}");
+
+Console.WriteLine();
+
+Console.WriteLine("Solving puzzle 2...");
+var (lastnumber, lastBoard) = FindLastBoard(boards, numbers);
+Console.WriteLine($"Last board to win has score {lastBoard.GetWinningScore(lastnumber)}");
+
+(int[], Board[]) ParseInput(string[] input) {
+    var numbers = input[0].Split(',').Select(int.Parse).ToArray();
+    var boards = new List<Board>();
+
+    var row = 2;
+    int id = 0;
+    while(true) {
+        var table = input.Skip(row).Take(5).ToArray();
+
+        if (table.Length == 0) break;
+
+        boards.Add(Board.From(table, id++));
+        row += 6;
+    }
+
+    return (numbers, boards.ToArray());
+}
+
+(int lastNumber, Board? winningBoard) Play(Board[] boards, int[] numbers) {
+    Board? winner = null;
+    int lastNumber = 0;
+    foreach (var number in numbers) {
+        foreach (var board in boards) { 
+            board.DrawNumber(number);
+            if (board.IsBingo()) {
+                winner = board; 
+                lastNumber = number;
+                break;
+            }            
+        }
+        if (winner != null) break;
+    }
+
+    return (lastNumber, winner);
+}
+
+(int lastNumber, Board? winningBoard) FindLastBoard(Board[] boards, int[] numbers) {
+    var boardsLeft = new List<Board>(boards).ToArray();
+
+    while(true) {
+        var (lastNumber, board) = Play(boardsLeft, numbers);
+        boardsLeft = new List<Board>(boardsLeft.Where(b => b.Id != board.Id)).ToArray();
+        if (boardsLeft.Length == 0) return (lastNumber, board);
+    }
+}

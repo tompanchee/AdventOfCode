8ccde19ec38af7b512fa661a42b3c609e45e82b3
Pruiksma, Tom <tom.pruiksma@keyloop.com>
2021-12-07 09:26:24 +0200

    Day 7

---
+++ 2021/Day07/Program.cs
@@ -0,0 +1,33 @@
+﻿var input = File.ReadAllText("input.txt");
+
+var crabs = input.Split(',').Select(int.Parse).ToArray();
+
+Console.WriteLine("Solving puzzle 1...");
+var minFuel = int.MaxValue;
+
+for(var pos = crabs.Min(); pos <= crabs.Max(); pos++) {
+    var fuel = 0;
+    foreach(var crab in crabs) {
+        fuel += Math.Abs(crab -pos);
+        if (fuel > minFuel) break;
+    }
+    if (fuel < minFuel) minFuel = fuel;
+}
+
+Console.WriteLine($"Minimum fuel to get aligned is {minFuel}");
+
+Console.WriteLine();
+
+Console.WriteLine("Solving puzzle 2...");
+minFuel = int.MaxValue;
+
+for(var pos = crabs.Min(); pos <= crabs.Max(); pos++) {
+    var fuel = 0;
+    foreach(var crab in crabs) {
+        fuel += Enumerable.Range(1, Math.Abs(crab -pos)).Sum();        
+        if (fuel > minFuel) break;
+    }
+    if (fuel < minFuel) minFuel = fuel;
+}
+
+Console.WriteLine($"Minimum fuel to get aligned is {minFuel}");

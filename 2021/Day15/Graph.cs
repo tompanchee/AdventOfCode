4a5830ccfbb2d1957e3f874d471ed2fa8015ffe7
Pruiksma, Tom <tom.pruiksma@keyloop.com>
2021-12-17 15:26:03 +0200

    Day 15

---
+++ 2021/Day15/Graph.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day15
+{
+    class Graph<T>
+    {
+        private readonly IDictionary<T, Vertex<T>> vertices;
+        private readonly int? logInterval;
+        private int visitedNodes;
+
+        public Graph(IList<Vertex<T>> vertices, int? logInterval = null) {
+            this.logInterval = logInterval;
+            this.vertices = vertices.ToDictionary(v => v.Id, v => v);
+        }
+
+        public (int lenght, List<T> path) FindShortestPath(T start, T end, Func<T,T,int> calculateHeuristicDistance) {
+            var workData = SetupWorkData(start, end, calculateHeuristicDistance);
+            var workQueue = new Dictionary<T, WorkVertex> {{start, workData.Single(v => v.Id.Equals(start)) }};
+            while (true) {
+                if (logInterval.HasValue && visitedNodes % logInterval == 0) Console.WriteLine($"Visited {visitedNodes} nodes");
+                //var current = workQueue.Values.Where(v => v.Visited == false && v.Cost != int.MaxValue).OrderBy(v => v.Cost).First();
+                var current = workQueue.Values.OrderBy(v => v.Cost).First();
+                if (current.Id.Equals(end)) break;
+                foreach (var (cost, neighbour) in vertices[current.Id].Neighbours) {
+                    var work = workData.Single(w => w.Id.Equals(neighbour));
+                    if (work.Visited) continue;
+                    if (work.PathLength == int.MaxValue || work.PathLength > current.PathLength + cost) {
+                        work.PathLength = current.PathLength + cost;
+                        work.Previous = current;
+                    }
+                    if (!workQueue.ContainsKey(work.Id)) workQueue.Add(work.Id, work);
+                }
+
+                current.Visited = true;
+                workQueue.Remove(current.Id);
+                visitedNodes++;
+            }
+
+            var path = new List<T>();
+            var c = workData.Single(w => w.Id.Equals(end));
+            var length = c.PathLength;
+            while (!c.Id.Equals(start)) {
+                path.Add(c.Id);
+                c = c.Previous;
+            }
+
+            path.Add(c.Id);
+            path.Reverse();
+
+            return (length, path);
+        }
+
+        private IList<WorkVertex> SetupWorkData(T start, T end, Func<T, T, int> calculateHeuristicDistance) {
+            var result = vertices.Keys.Select(id => new WorkVertex {
+                Id = id,
+                HeuristicDistance = calculateHeuristicDistance(end, id)
+            }).ToList();
+            result.Single(v => v.Id.Equals(start)).PathLength = 0;
+            return result;
+        }
+
+        class WorkVertex
+        {
+            public T Id { get; init; }
+            public int PathLength { get; set; } = int.MaxValue;
+            public WorkVertex Previous { get; set; }
+            public bool Visited { get; set; }
+            public int HeuristicDistance { get; set; }
+            public int Cost => PathLength == int.MaxValue ? int.MaxValue : PathLength + HeuristicDistance;
+        }
+    }
+
+    class Vertex<T>
+    {
+        public Vertex(T id) {
+            Id = id;
+        }
+
+        public T Id { get; }
+
+        public IList<(int cost, T vertex)> Neighbours { get; } = new List<(int cost, T vertex)>();
+    }
+}
---
+++ 2021/Day15/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var data = File.ReadAllLines("input.txt");
+
+            Console.WriteLine("Solving puzzle 1...");
+            var sw = Stopwatch.StartNew();
+            var graph = BuildGraph(data);
+            var (risk, _) = graph.FindShortestPath((0, 0), (data.Length - 1, data[0].Length - 1), Euclidean);
+            Console.WriteLine($"Minimum total risk through cave is {risk} in {sw.Elapsed}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Solving puzzle 2...");
+            sw.Restart();
+            graph = BuildGraph5x(data);
+            Console.WriteLine($"Graph built, calculating path, time elapsed {sw.Elapsed}");
+            (risk, _) = graph.FindShortestPath((0, 0), (data.Length * 5 - 1, data[0].Length * 5 - 1), Euclidean);
+            Console.WriteLine($"Minimum total risk through cave is {risk} in {sw.Elapsed}");
+        }
+
+        static int NoHeuristics((int row, int col) n1, (int row, int col) n2) => 0;
+        static int Manhattan((int row, int col) n1, (int row, int col) n2) => Math.Abs(n1.row - n2.row) + Math.Abs(n1.col - n2.col);
+        static int Euclidean((int row, int col) n1, (int row, int col) n2) => (int)Math.Sqrt(Math.Pow(n1.row - n2.row, 2) + Math.Pow(n1.col - n2.col, 2));
+
+        private static Graph<(int row, int col)> BuildGraph(string[] data) {
+            (int dr, int dc)[] offsets = {
+                (-1, 0),
+                (0, 1),
+                (1, 0),
+                (0, -1)
+            };
+
+            var vertices = new List<Vertex<(int row, int col)>>();
+
+            for (var row = 0; row < data.Length; row++) {
+                for (var col = 0; col < data[row].Length; col++) {
+                    vertices.Add(new Vertex<(int row, int col)>((row, col)));
+                }
+            }
+
+            foreach (var vertex in vertices) {
+                foreach (var (dr, dc) in offsets) {
+                    var nr = vertex.Id.row + dr;
+                    var nc = vertex.Id.col + dc;
+
+                    if (nr < 0 || nr > data.Length - 1 || nc < 0 || nc > data[0].Length - 1) continue;
+
+                    vertex.Neighbours.Add((int.Parse(data[nr][nc].ToString()), (nr, nc)));
+                }
+            }
+
+            return new Graph<(int row, int col)>(vertices, 100);
+        }
+
+        private static Graph<(int row, int col)> BuildGraph5x(string[] data) {
+            (int dr, int dc)[] offsets = {
+                (-1, 0),
+                (0, 1),
+                (1, 0),
+                (0, -1)
+            };
+
+            var vertices = new List<Vertex<(int row, int col)>>();
+
+            for (var row = 0; row < data.Length * 5; row++) {
+                for (var col = 0; col < data[row % 100].Length * 5; col++) {
+                    vertices.Add(new Vertex<(int row, int col)>((row, col)));
+                }
+            }
+
+            foreach (var vertex in vertices) {
+                foreach (var (dr, dc) in offsets) {
+                    var nr = vertex.Id.row + dr;
+                    var nc = vertex.Id.col + dc;
+
+                    if (nr < 0 || nr > data.Length * 5 - 1 || nc < 0 || nc > data[0].Length * 5 - 1) continue;
+
+                    var nrCost = int.Parse(data[nr % 100][nc % 100].ToString());
+                    nrCost += nr / 100 + nc / 100;
+                    if (nrCost > 9) nrCost -= 9;
+
+                    vertex.Neighbours.Add((nrCost, (nr, nc)));
+                }
+            }
+
+            return new Graph<(int row, int col)>(vertices, 2500);
+        }
+    }
+}

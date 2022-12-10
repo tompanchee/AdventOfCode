5ac0b91a048ff0881ed25f0d179d98c50772ce0c
Tom Pruiksma <tom.pruiksma@keyloop.com>
2022-12-10 10:56:50 +0200

    2022 - day 10

---
+++ 2022/Solutions/Day10/Device.cs
@@ -0,0 +1,58 @@
+﻿using Serilog.Core;
+
+namespace Day10;
+
+internal class Device
+{
+    readonly Dictionary<int, Action<int, int>> cycleEvents = new();
+    readonly Action<int, int>? everyCycleEvent;
+    readonly Logger logger;
+
+    int cycle;
+    int x = 1;
+
+    public Device(Logger logger, Action<int, int>? everyCycleEvent = null) {
+        this.logger = logger;
+        this.everyCycleEvent = everyCycleEvent;
+    }
+
+    public void Run(string[] program) {
+        foreach (var operation in program) {
+            logger.Debug("Operation: {operation}", operation);
+            var split = operation.Split(' ');
+
+            switch (split[0]) {
+                case "noop":
+                    NoOperation();
+                    break;
+                case "addx":
+                    AddX(split[1]);
+                    break;
+                default:
+                    throw new InvalidOperationException($"Invalid operation : {operation}");
+            }
+        }
+    }
+
+    void NoOperation() {
+        IncrementCycle();
+    }
+
+    void AddX(string value) {
+        IncrementCycle();
+        IncrementCycle();
+        x += int.Parse(value);
+    }
+
+    void IncrementCycle() {
+        cycle++;
+        if (cycleEvents.ContainsKey(cycle)) cycleEvents[cycle](cycle, x);
+        everyCycleEvent?.Invoke(cycle, x);
+
+        logger.Debug("Cycle: {cycle}, x: {x}", cycle, x);
+    }
+
+    public void Subscribe(int c, Action<int, int> callback) {
+        cycleEvents.Add(c, callback);
+    }
+}
---
+++ 2022/Solutions/Day10/Solver.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using AOCCommon;
+using Serilog.Core;
+
+namespace Day10;
+
+[Day(10, "Cathode-Ray Tube")]
+internal class Solver : SolverBase
+{
+    public Solver(string path, Logger logger) : base(path, logger) { }
+
+    public override void Solve1() {
+        var cycleValues = new Dictionary<int, int>();
+        var device = new Device(logger);
+
+        device.Subscribe(20, RegisterValues);
+        device.Subscribe(60, RegisterValues);
+        device.Subscribe(100, RegisterValues);
+        device.Subscribe(140, RegisterValues);
+        device.Subscribe(180, RegisterValues);
+        device.Subscribe(220, RegisterValues);
+
+        device.Run(data);
+
+        var sum = cycleValues.Sum(v => v.Key * v.Value);
+        logger.Information("Sum of required signal strengths is {sum}", sum);
+
+        void RegisterValues(int cycle, int x) {
+            cycleValues.Add(cycle, x);
+        }
+    }
+
+    public override void Solve2() {
+        var sb = new StringBuilder();
+        var currentRow = -1;
+
+        var device = new Device(logger, RegisterValue);
+
+        device.Run(data);
+
+        logger.Information(sb.ToString());
+
+        void RegisterValue(int cycle, int x) {
+            var row = (cycle - 1) / 40;
+            var col = (cycle - 1) % 40;
+
+            if (row != currentRow) {
+                currentRow = row;
+                sb.AppendLine();
+            }
+
+            sb.Append(Math.Abs(col - x) < 2 ? '#' : '.');
+        }
+    }
+
+    protected override void PostConstruct() {
+        logger.Information("Reading program...");
+    }
+}

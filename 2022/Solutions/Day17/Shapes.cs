1c3c7a7a5a2c15cb01029599743b8acc9675912c
Tom Pruiksma <tom.pruiksma@keyloop.com>
2023-01-04 09:40:04 +0200

    2022 - day 17 (part 1)

---
+++ 2022/Solutions/Day17/Shapes.cs
@@ -0,0 +1,59 @@
+﻿namespace Day17;
+
+internal static class Shapes
+{
+    // Flip shapes horizontally to help with indexes, bottom index 0
+
+    // ####
+    static readonly byte[] shape1 = {
+        0b00011110
+    };
+
+    // .#.
+    // ###
+    // .#.
+    static readonly byte[] shape2 = {
+        0b00001000,
+        0b00011100,
+        0b00001000
+    };
+
+    // ..#
+    // ..#
+    // ###
+    static readonly byte[] shape3 = {
+        0b00011100,
+        0b00000100,
+        0b00000100
+    };
+
+    // #
+    // #
+    // #
+    // #
+    static readonly byte[] shape4 = {
+        0b00010000,
+        0b00010000,
+        0b00010000,
+        0b00010000
+    };
+
+    // ##
+    // ##
+    static readonly byte[] shape5 = {
+        0b00011000,
+        0b00011000
+    };
+
+    static readonly IList<byte[]> shapeList = new List<byte[]> {
+        shape1,
+        shape2,
+        shape3,
+        shape4,
+        shape5
+    };
+
+    public static byte[] GetShape(int i) {
+        return shapeList[i % shapeList.Count].ToArray();
+    }
+}
---
+++ 2022/Solutions/Day17/Solver.cs
@@ -0,0 +1,110 @@
+﻿using AOCCommon;
+using Serilog.Core;
+
+namespace Day17;
+
+[Day(17, "Pyroclastic Flow")]
+internal class Solver : SolverBase
+{
+    List<byte> chamber = new() {
+        0b11111111 // Floor
+    };
+
+    int instructionPos;
+
+    string? jetPattern;
+
+    public Solver(string path, Logger logger) : base(path, logger) { }
+
+    public override void Solve1() {
+        logger.Information("Dropping rocks...");
+
+        for (var i = 0; i < 2022; i++) {
+            var shape = Shapes.GetShape(i);
+            DropRock(shape);
+        }
+
+        logger.Information("The tower is {0} units tall", chamber.Count - 1);
+    }
+
+    public override void Solve2() {
+        logger.Information("Dropping even more rocks...");
+    }
+
+    protected override void PostConstruct() {
+        logger.Information("Reading jet pattern...");
+
+        jetPattern = data[0];
+        //jetPattern = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
+    }
+
+    void DropRock(byte[] shape) {
+        var topRock = chamber.Count - 1;
+        var rockBottom = topRock + 4;
+        var settled = false;
+        while (!settled) {
+            var instruction = GetNextInstruction();
+            if (instruction == '<') MoveLeft();
+            else MoveRight();
+            MoveDown();
+        }
+
+        void MoveLeft() {
+            // Check for edges
+            if (shape.Any(s => (s & 64) == 64)) return; // Already at edge
+
+            // Shift to new position
+            var newShape = new byte[shape.Length];
+            for (var i = 0; i < shape.Length; i++) newShape[i] = (byte) (shape[i] << 1);
+
+            // Check for overlaps with existing rows
+            if (IsOverlapping(newShape, rockBottom)) return;
+
+            shape = newShape;
+        }
+
+        void MoveRight() {
+            // Check for edges
+            if (shape.Any(s => (s & 1) == 1)) return; // Already at edge
+            //if (rockBottom > chamberHeight) return; // Not yet at bottom
+
+            // Shift to new position
+            var newShape = new byte[shape.Length];
+            for (var i = 0; i < shape.Length; i++) newShape[i] = (byte) (shape[i] >> 1);
+
+            // Check for overlaps with existing rows
+            if (IsOverlapping(newShape, rockBottom)) return;
+
+            shape = newShape;
+        }
+
+        void MoveDown() {
+            var newBottom = rockBottom - 1;
+            // Test for overlaps
+            if (IsOverlapping(shape, newBottom)) {
+                settled = true;
+                for (var i = 0; i < shape.Length; i++)
+                    if (rockBottom + i <= topRock) chamber[rockBottom + i] = (byte) (chamber[rockBottom + i] | shape[i]);
+                    else chamber.Add(shape[i]);
+                return;
+            }
+
+            rockBottom = newBottom;
+        }
+
+        bool IsOverlapping(IReadOnlyList<byte> s, int bottom) {
+            for (var i = 0; i < topRock - bottom + 1; i++) {
+                if (i >= shape.Length) return false;
+                if ((chamber[bottom + i] & s[i]) != 0) return true;
+            }
+
+            return false;
+        }
+    }
+
+    char GetNextInstruction() {
+        var i = jetPattern![instructionPos++];
+        if (instructionPos >= jetPattern.Length) instructionPos = 0;
+        return i;
+    }
+}

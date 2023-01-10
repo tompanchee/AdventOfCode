1ce0bb9f919438208b16f4d59e441004099c4b06
Tom Pruiksma <tom.pruiksma@keyloop.com>
2023-01-10 16:30:35 +0200

    2022 - Day 25

---
+++ 2022/Solutions/Day25/SNAFUConverter.cs
@@ -0,0 +1,64 @@
+﻿namespace Day25;
+
+internal static class SNAFUConverter
+{
+    public static string LongToSNAFU(this long value) {
+        var result = "";
+        while (value > 0) {
+            var remainder = value % 5;
+            if (remainder > 2) remainder -= 5;
+            switch (remainder) {
+                case 0:
+                case 1:
+                case 2:
+                    result = $"{remainder}{result}";
+                    break;
+                case -1:
+                    result = $"-{result}";
+                    break;
+                case -2:
+                    result = $"={result}";
+                    break;
+            }
+
+            value -= remainder;
+            value /= 5;
+        }
+
+        return result;
+    }
+
+    public static long SNAFUToLong(this string? value) {
+        value = new string(value?.Reverse().ToArray());
+        var pos = 0;
+        var result = 0L;
+        foreach (var digit in value) {
+            switch (digit) {
+                case '0':
+                    break;
+                case '1':
+                    result += Power(5, pos);
+                    break;
+                case '2':
+                    result += 2 * Power(5, pos);
+                    break;
+                case '-':
+                    result -= Power(5, pos);
+                    break;
+                case '=':
+                    result -= 2 * Power(5, pos);
+                    break;
+            }
+
+            pos++;
+        }
+
+        return result;
+    }
+
+    static long Power(long number, int power) {
+        var result = 1L;
+        for (var i = 0; i < power; i++) result *= number;
+        return result;
+    }
+}
---
+++ 2022/Solutions/Day25/Solver.cs
@@ -0,0 +1,26 @@
+﻿using AOCCommon;
+using Serilog.Core;
+
+namespace Day25;
+
+[Day(25, "Full of Hot Air")]
+internal class Solver : SolverBase
+{
+    public Solver(string path, Logger logger) : base(path, logger) { }
+
+    public override void Solve1() {
+        logger.Information("Calculating sum...");
+        var sum = data.Where(snafu => !string.IsNullOrWhiteSpace(snafu)).Sum(snafu => snafu.SNAFUToLong());
+
+        logger.Information("Resolving sum in SNAFU...");
+        var result = sum.LongToSNAFU();
+
+        logger.Information("Number to input to Bob's console is {result}", result);
+    }
+
+    public override void Solve2() { }
+
+    protected override void PostConstruct() {
+        logger.Information("Reading numbers...");
+    }
+}

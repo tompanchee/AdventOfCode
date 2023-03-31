5e01fd183e982ed3fa232c6f97e8839e9ade4239
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2023-03-31 12:01:37 +0300

    2017 - Day 10

---
+++ 2017/Day10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Day10;
+
+var data = File.ReadAllLines("input.txt")[0];
+var input = data
+    .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine("Solving part 1...");
+
+var hasher = KnotHasher.Init();
+hasher.Scramble(input);
+
+Console.WriteLine($"Product of first two numbers is {hasher[0] * hasher[1]}");
+
+Console.WriteLine();
+
+Console.WriteLine("Solving part 2...");
+
+var lengths = Encoding.ASCII.GetBytes(data)
+    .Select(Convert.ToInt32)
+    .ToList();
+lengths.AddRange(new[] { 17, 31, 73, 47, 23 });
+
+hasher = KnotHasher.Init(); // reset hasher
+hasher.Scramble(lengths.ToArray(), 64);
+var hash = hasher.CalculateHash();
+
+Console.WriteLine($"The knot hash is {hash}");

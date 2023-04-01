f27926f19aeaeb88ead67d31b5ba42c2e00b3b01
Tom Pruiksma <tom.pruiksma@gmail.com>
2023-04-01 16:18:58 +0300

    2017 - Day 11

---
+++ 2017/Day11/Hex.cs
@@ -0,0 +1,49 @@
+﻿namespace Day11;
+
+class Hex
+{
+    // Axial coordinate system, see  https://www.redblobgames.com/grids/hexagons/#coordinates-axial
+    public Hex(int q, int r)
+    {
+        Q = q;
+        R = r;
+    }
+
+    public int Q { get; private set; }
+    public int R { get; private set; }
+
+    public void Move(string direction)
+    {
+       switch (direction)
+        {
+            case "n":
+                R--;
+                break;            
+            case "ne":
+                R--;
+                Q++;
+                break;
+            case "se":
+                Q++;                
+                break;
+            case "s":
+                R++;
+                break;
+            case "sw":
+                Q--;
+                R++;
+                break;
+            case "nw":
+                Q--;
+                break;
+                
+        }
+    }
+
+    public int DistanceTo(Hex other)
+    {
+        return (Math.Abs(Q - other.Q)
+          + Math.Abs(Q + R - other.Q - other.R)
+          + Math.Abs(R - other.R)) / 2;
+    }
+}
---
+++ 2017/Day11/Program.cs
@@ -0,0 +1,25 @@
+﻿using Day11;
+
+var input = File.ReadAllLines("input.txt")[0];
+var data = input.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+Console.Write("Solving part 1...");
+
+var child = new Hex(0, 0);
+foreach (var direction in data) child.Move(direction);
+
+Console.WriteLine($"Distance to child process is {child.DistanceTo(new Hex(0, 0))}");
+
+Console.WriteLine();
+
+Console.WriteLine("Solving part 2...");
+var maxDistance = int.MinValue;
+
+child = new Hex(0, 0);
+foreach (var direction in data) { 
+    var d = child.DistanceTo(new Hex(0, 0));
+    if (d > maxDistance) maxDistance = d;
+    child.Move(direction); 
+}
+
+Console.WriteLine($"Max distance to child process was {maxDistance}");

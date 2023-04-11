212086b5eafc40a6d0b71e06e846ea383b38f9f0
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2023-04-11 16:29:12 +0300

    2017 - Days 16 and 17

---
+++ 2017/Day16/Program.cs
@@ -0,0 +1,70 @@
+﻿var input = File.ReadAllLines("input.txt").First();
+var moves = input.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+Console.WriteLine("Solving part 1...");
+
+var programs = moves.Aggregate("abcdefghijklmnop", Move);
+
+Console.WriteLine($"Programs after dancing is {programs}");
+
+Console.WriteLine();
+
+Console.WriteLine("Solving puzzle 2...");
+// Look for sequences while dancing
+programs = "abcdefghijklmnop";
+
+var statuses = new List<string>();
+int start;
+int end;
+var count = 0;
+
+while (true) {
+    programs = moves.Aggregate(programs, Move);
+
+    if (statuses.Contains(programs)) {
+        var statusIdx = statuses.IndexOf(programs);
+        start = statusIdx;
+        end = count;
+        break;
+    }
+
+    count++;
+    statuses.Add(programs);
+}
+
+var wantedIndex = (1_000_000_000 - start) % (end - start) + start - 1;
+
+Console.WriteLine($"After 1000000000 moves the programs is {statuses[wantedIndex]}");
+
+string Move(string value, string instruction) {
+    return instruction[0] switch {
+        's' => Spin(value),
+        'x' => Exchange(value),
+        'p' => Partner(value),
+        _ => throw new InvalidOperationException()
+    };
+
+    string Spin(string s) {
+        var spin = int.Parse(instruction[1..]);
+        var e = s[^spin..];
+        return $"{e}{s[..^spin]}";
+    }
+
+    string Exchange(string s) {
+        var split = instruction[1..].Split('/');
+        var p1 = int.Parse(split[0]);
+        var p2 = int.Parse(split[1]);
+        var a = s.ToCharArray();
+        (a[p1], a[p2]) = (a[p2], a[p1]);
+        return new string(a);
+    }
+
+    string Partner(string s) {
+        var split = instruction[1..].Split('/');
+        var p1 = s.IndexOf(split[0], StringComparison.Ordinal);
+        var p2 = s.IndexOf(split[1], StringComparison.Ordinal);
+        var a = s.ToCharArray();
+        (a[p1], a[p2]) = (a[p2], a[p1]);
+        return new string(a);
+    }
+}
---
+++ 2017/Day17/Program.cs
@@ -0,0 +1,51 @@
+﻿var input = 303;
+
+Console.WriteLine("Solving puzzle 1...");
+
+var sl = new Spinlock();
+
+var current = 0;
+var pos = 0;
+while (sl.Length < 2018) {
+    pos = sl.GetIndex(pos + input);
+    sl.InsertAfterPos(pos, ++current);
+    pos++;
+}
+
+Console.WriteLine($"Value after the last value is {sl[pos + 1]}");
+
+Console.WriteLine();
+
+Console.WriteLine("Solving puzzle 2...");
+
+// Keep track on pos 1
+current = 0;
+pos = 0;
+var length = 1;
+var valueAt1 = -1;
+
+while (length < 50000001) {
+    current++;
+    pos = (pos + input) % length;
+    if (++pos == 1) valueAt1 = current;
+    length++;
+}
+
+Console.WriteLine($"Value after zero is {valueAt1}");
+
+internal class Spinlock
+{
+    readonly List<int> inner = new() { 0 };
+
+    public int Length => inner.Count;
+
+    public int this[int i] => inner[i % inner.Count];
+
+    public int GetIndex(int idx) {
+        return idx % inner.Count;
+    }
+
+    public void InsertAfterPos(int pos, int value) {
+        inner.Insert(pos + 1, value);
+    }
+}

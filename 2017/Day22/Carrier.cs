9f4b3e9df797f0a65386e2788bae492a3d87305c
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2023-06-09 15:12:50 +0300

    2017 Day 22 - part 1

---
+++ 2017/Day22/Carrier.cs
@@ -0,0 +1,62 @@
+﻿namespace Day22;
+
+internal class Carrier
+{
+    public enum TurnDirection
+    {
+        Left,
+        Right
+    }
+
+    Direction direction = Direction.Up;
+    int x;
+    int y;
+
+    public (int x, int y) Location => (x, y);
+
+    public void Move() {
+        switch (direction) {
+            case Direction.Up:
+                y++;
+                break;
+            case Direction.Down:
+                y--;
+                break;
+            case Direction.Left:
+                x--;
+                break;
+            case Direction.Right:
+                x++;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public void Turn(TurnDirection td) {
+        if (td == TurnDirection.Left)
+            direction = direction switch {
+                Direction.Up => Direction.Left,
+                Direction.Right => Direction.Up,
+                Direction.Down => Direction.Right,
+                Direction.Left => Direction.Down,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        else
+            direction = direction switch {
+                Direction.Up => Direction.Right,
+                Direction.Right => Direction.Down,
+                Direction.Down => Direction.Left,
+                Direction.Left => Direction.Up,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+    }
+
+    enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+}
---
+++ 2017/Day22/Program.cs
@@ -0,0 +1,48 @@
+﻿using Day22;
+
+Console.WriteLine("Scanning grid...");
+
+var infectedNodes = new HashSet<(int x, int y)>();
+
+var data = File.ReadAllLines("input.txt").Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+
+//data = new[] {
+//    "..#",
+//    "#..",
+//    "..."
+//};
+
+var y = data.Length / 2;
+var xmax = data[0].Length / 2;
+
+foreach (var line in data) {
+    for (var x = 0; x < data.Length; x++)
+        if (line[x] == '#')
+            infectedNodes.Add((x - xmax, y));
+
+    y--;
+}
+
+var carrier = new Carrier();
+
+Console.WriteLine();
+Console.WriteLine("Solving part 1...");
+var count = 0;
+for (var i = 0; i < 10000; i++)
+    if (Burst())
+        count++;
+Console.WriteLine($"{count} bursts caused infection");
+
+bool Burst() {
+    if (infectedNodes!.Contains(carrier!.Location)) {
+        carrier.Turn(Carrier.TurnDirection.Right);
+        infectedNodes.Remove(carrier.Location);
+        carrier.Move();
+        return false;
+    }
+
+    carrier.Turn(Carrier.TurnDirection.Left);
+    infectedNodes.Add(carrier.Location);
+    carrier.Move();
+    return true;
+}

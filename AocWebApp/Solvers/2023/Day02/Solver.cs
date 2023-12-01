12b2d833e389ab1d06d86c04eac5118ea33bfc27
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2023-12-01 15:48:27 +0200

    Stub for day 2, enable try it out and hide curl command

---
+++ AocWebApp/Solvers/2023/Day02/Solver.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using Common.Solver;
+using Serilog;
+
+namespace Day02;
+
+[Day(2023, 2, "TODO:")]
+internal class Solver : SolverBase
+{
+    public Solver(string input, ILogger logger) : base(input, logger) { }
+
+    public override Task Solve1()
+    {
+        return Task.CompletedTask;
+    }
+
+    public override Task Solve2()
+    {
+        return Task.CompletedTask;
+    }
+}

f337662c81ac6cc971859fc2a10e8e6266afb04f
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2023-12-02 10:32:39 +0200

    2023 - day 2

---
+++ AocWebApp/Solvers/2023/Day02/Game.cs
@@ -0,0 +1,73 @@
+﻿namespace Day02;
+
+class Game
+{
+    private const int MaxRed = 12;
+    private const int MaxGreen = 13;
+    private const int MaxBlue = 14;
+
+    readonly private List<Round> rounds;
+
+    private Game(int id, List<Round> rounds)
+    {
+        ID = id;
+        this.rounds = rounds;
+    }
+
+    public static Game FromInput(string input)
+    {
+        var idx = input.IndexOf(':');
+        var id = int.Parse(input[4..idx]);
+
+        var roundsInput = input[(idx + 1)..];
+        var split = roundsInput.Split(';');
+        var rounds = new List<Round>();
+        foreach (string color in split)
+        {
+            var colors = color.Split(',', StringSplitOptions.TrimEntries);
+            var red = 0;
+            var green = 0;
+            var blue = 0;
+            foreach (string col in colors)
+            {
+                var idx2 = col.IndexOf(' ');
+                var c = col[(idx2 + 1)..];
+                var amount = int.Parse(col[..idx2]);
+                switch (c)
+                {
+                    case "red":
+                        red = amount;
+                        break;
+                    case "green":
+                        green = amount;
+                        break;
+                    case "blue":
+                        blue = amount;
+                        break;
+                }
+            }
+
+            rounds.Add(new Round(red, green, blue));
+        }
+
+        return new Game(id, rounds);
+    }
+
+    public int ID { get; }
+
+    public bool IsImpossible()
+    {
+        return rounds.Any(round => round.Red > MaxRed || round.Blue > MaxBlue || round.Green > MaxGreen);
+    }
+
+    public int Power()
+    {
+        var red = rounds.Select(r => r.Red).Max();
+        var green = rounds.Select(r => r.Green).Max();
+        var blue = rounds.Select(r => r.Blue).Max();
+
+        return red * green * blue;
+    }
+
+    record Round(int Red, int Green, int Blue);
+}
--- AocWebApp/Solvers/2023/Day02/Solver.cs
+++ AocWebApp/Solvers/2023/Day02/Solver.cs
@@ -4,18 +4,35 @@ using Serilog;
 
 namespace Day02;
 
-[Day(2023, 2, "TODO:")]
+[Day(2023, 2, "Cube Conundrum")]
 internal class Solver : SolverBase
 {
-    public Solver(string input, ILogger logger) : base(input, logger) { }
+    private readonly List<Game> games = new();
+
+    public Solver(string input, ILogger logger) : base(input, logger)
+    {
+        logger.Information("Playing games...");
+
+        foreach (string line in inputLines.Where(l => !string.IsNullOrWhiteSpace(l)))
+        {
+            games.Add(Game.FromInput(line));
+        }
+    }
 
     public override Task Solve1()
     {
+        var sum = games.Where(g => !g.IsImpossible()).Sum(g => g.ID);
+        logger.Information("Sum of possible game Ids is {sum}", sum);
+
         return Task.CompletedTask;
     }
 
     public override Task Solve2()
     {
+        logger.Information("Calculating power...");
+        var sum = games.Sum(g => g.Power());
+        logger.Information("Total power is {sum}", sum);
+
         return Task.CompletedTask;
     }
 }

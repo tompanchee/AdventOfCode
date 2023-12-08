da608db1db690f1417e4f2ae73808031e5ebf3a8
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2023-12-08 12:26:00 +0200

    2023 - day 8

---
+++ AocWebApp/Solvers/2023/Day08/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace Day08;
+
+internal class Node
+{
+    private Node(string name, string left, string right)
+    {
+        Name = name;
+        Left = left;
+        Right = right;
+    }
+
+    public static Node FromInput(string input)
+    {
+        var name = input[..3];
+        var split = input[7..^1].Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        return new Node(name, split[0], split[1]);
+    }
+
+    public string Name { get; }
+    public string Left { get; }
+    public string Right { get; }
+}
---
+++ AocWebApp/Solvers/2023/Day08/Solver.cs
@@ -0,0 +1,93 @@
+﻿using Common;
+using Common.Solver;
+using MathNet.Numerics;
+using Serilog;
+
+namespace Day08;
+
+[Day(2023, 8, "Haunted Wasteland")]
+internal class Solver : SolverBase
+{
+    private readonly string instructions;
+    private readonly Dictionary<string, Node> network = new();
+
+    public Solver(string input, ILogger logger) : base(input, logger)
+    {
+        logger.Information("Reading map...");
+        instructions = inputLines[0];
+
+        foreach (string line in inputLines[1..].Where(l => !string.IsNullOrWhiteSpace(l)))
+        {
+            var node = Node.FromInput(line);
+            network[node.Name] = node;
+        }
+    }
+
+    public override Task Solve1()
+    {
+        logger.Information("Navigating through network...");
+
+        var current = network["AAA"];
+        var pos = 0;
+        var count = 0;
+        while (current.Name != "ZZZ")
+        {
+            current = instructions[pos] == 'L' ? network[current.Left] : network[current.Right];
+
+            count++;
+            pos++;
+            if (pos == instructions.Length) pos = 0;
+        }
+
+        logger.Information("Getting to ZZZ takes {count} steps", count);
+
+        return Task.CompletedTask;
+    }
+
+    public override Task Solve2()
+    {
+        logger.Information("Navigating multiple nodes...");
+
+        var nodes = network.Where(n => n.Key[2] == 'A').Select(n => n.Value).ToList();
+        var sequences = nodes.Select(FindSequenceLengthForNode).ToList();
+
+        // Because the start offset and the sequence length are same, a simple LCM is enough
+        // Using LCM from https://www.nuget.org/packages/MathNet.Numerics/
+        var lcm = sequences.First().sequenceLentgth;
+        foreach ((var _, long length) in sequences.Skip(1))
+        {
+            lcm = Euclid.LeastCommonMultiple(lcm, length);
+        }
+
+        logger.Information("Getting all paths to **Z takes {count} steps", lcm);
+
+        return Task.CompletedTask;
+
+        (long offset, long sequenceLentgth) FindSequenceLengthForNode(Node node)
+        {
+            var current = node;
+            var pos = 0;
+            var count = 0;
+
+            var start = 0;
+            var end = 0;
+
+            while (start == 0 || end == 0)
+            {
+                current = instructions[pos] == 'L' ? network[current.Left] : network[current.Right];
+                count++;
+
+                if (current.Name[2] == 'Z')
+                {
+                    if (start == 0) start = count;
+                    else end = count;
+                }
+
+                pos++;
+                if (pos == instructions.Length) pos = 0;
+            }
+
+            return (start, end - start);
+        }
+    }
+}

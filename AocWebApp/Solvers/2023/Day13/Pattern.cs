bdafa4a7b1bcb6b9c3d847943ec077fac5a87d3b
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2024-01-02 10:29:25 +0200

    2023 Day 13

---
+++ AocWebApp/Solvers/2023/Day13/Pattern.cs
@@ -0,0 +1,77 @@
+﻿namespace Day13;
+
+internal class Pattern
+{
+    public Pattern(IEnumerable<string> input)
+    {
+        Rows = input.ToList();
+    }
+
+    public (List<int> rows, List<int> cols) GetLinesOfReflection()
+    {
+        var r = new List<int>();
+        var c = new List<int>();
+
+        for (var col = 1; col < Rows[0].Length; col++)
+        {
+            if (IsMirrorAtColumn(col))
+            {
+                c.Add(col);
+            }
+        }
+
+        for (var row = 1; row < Rows.Count; row++)
+        {
+            if (IsMirrorAtRow(row))
+            {
+                r.Add(row);
+            }
+        }
+
+        return (r, c);
+    }
+
+    public void Swap(int row, int col)
+    {
+        var c = Rows[row][col];
+        var oldRow = Rows[row].ToCharArray();
+        oldRow[col] = c == '.' ? '#' : '.';
+        var newRow = new string(oldRow);
+        Rows[row] = newRow;
+    }
+
+    private bool IsMirrorAtColumn(int col)
+    {
+        var count = Math.Min(col, Rows[0].Length - col);
+
+        foreach (var row in Rows)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                if (row[col - i - 1] != row[col + i]) return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool IsMirrorAtRow(int row)
+    {
+        var count = Math.Min(row, Rows.Count - row);
+
+        for (var i = 0; i < count; i++)
+        {
+            for (var c = 0; c < Rows[0].Length; c++)
+            {
+                if (Rows[row - i - 1][c] != Rows[row + i][c]) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public List<string> Rows { get; }
+
+    public int? ColumnOfReflection { get; set; }
+    public int? RowOfReflection { get; set; }
+}
---
+++ AocWebApp/Solvers/2023/Day13/Solver.cs
@@ -0,0 +1,101 @@
+﻿using Common;
+using Common.Solver;
+using Serilog;
+
+namespace Day13;
+
+[Day(2023, 13, "Point of Incidence")]
+internal class Solver : SolverBase
+{
+    private readonly List<Pattern> patterns = new();
+
+    public Solver(string input, ILogger logger) : base(input, logger)
+    {
+        logger.Information("Scanning mirrors...");
+
+        var data = new List<string>();
+        foreach (string line in inputLines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                if (data.Count > 0) patterns.Add(new Pattern(data));
+                data.Clear();
+            }
+            else
+            {
+                data.Add(line);
+            }
+        }
+
+        if (data.Count > 0) patterns.Add(new Pattern(data));
+    }
+
+    public override Task Solve1()
+    {
+        logger.Information("Positioning mirrors...");
+
+        foreach (var pattern in patterns)
+        {
+            (List<int> rows, List<int> cols) = pattern.GetLinesOfReflection();
+
+            if (rows.Any()) pattern.RowOfReflection = rows[0];
+            if (cols.Any()) pattern.ColumnOfReflection = cols[0];
+        }
+
+        var sum = patterns.Where(p => p.RowOfReflection != null).Sum(p => p.RowOfReflection) * 100
+                  + patterns.Where(p => p.ColumnOfReflection != null).Sum(p => p.ColumnOfReflection);
+        logger.Information("Total mirror sum is {sum}", sum);
+
+        return Task.CompletedTask;
+    }
+
+    public override Task Solve2()
+    {
+        logger.Information("Cleaning up smudges...");
+
+        foreach (var pattern in patterns)
+        {
+            var found = false;
+            for (var r = 0; r < pattern.Rows.Count; r++)
+            {
+                for (var c = 0; c < pattern.Rows[r].Length; c++)
+                {
+                    pattern.Swap(r, c);
+                    (List<int> rows, List<int> cols) = pattern.GetLinesOfReflection();
+
+                    if (rows.Any())
+                    {
+                        if (rows.Any(row => row != pattern.RowOfReflection))
+                        {
+                            pattern.RowOfReflection = rows.First(row => row != pattern.RowOfReflection);
+                            pattern.ColumnOfReflection = null;
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (cols.Any())
+                    {
+                        if (cols.Any(col => col != pattern.ColumnOfReflection))
+                        {
+                            pattern.ColumnOfReflection = cols.First(col => col != pattern.ColumnOfReflection);
+                            pattern.RowOfReflection = null;
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    pattern.Swap(r, c); // Reset 
+                }
+
+                if (found) break;
+            }
+        }
+
+        var sum = patterns.Where(p => p.RowOfReflection != null).Sum(p => p.RowOfReflection) * 100
+                  + patterns.Where(p => p.ColumnOfReflection != null).Sum(p => p.ColumnOfReflection);
+        logger.Information("Total mirror sum is {sum}", sum);
+
+        return Task.CompletedTask;
+    }
+}

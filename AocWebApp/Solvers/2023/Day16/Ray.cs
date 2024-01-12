858dc80dd33214233c3e7adc05e6c671221dcb23
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2024-01-12 14:20:12 +0200

    2023 - Day 16

---
+++ AocWebApp/Solvers/2023/Day16/Ray.cs
@@ -0,0 +1,87 @@
+﻿namespace Day16;
+
+internal class Ray
+{
+    private (int x, int y) position;
+    private EDirection direction;
+
+    public Ray((int x, int y) position, EDirection direction)
+    {
+        this.position = position;
+        this.direction = direction;
+    }
+
+    public void Move()
+    {
+        switch (direction)
+        {
+            case EDirection.North:
+                position.y--;
+                break;
+            case EDirection.South:
+                position.y++;
+                break;
+            case EDirection.West:
+                position.x--;
+                break;
+            case EDirection.East:
+                position.x++;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public bool IsOutOfBounds(string[] cave)
+    {
+        if (position.x < 0 || position.y < 0) return true;
+        if (position.x > cave[0].Length - 1) return true;
+        if (position.y > cave.Length - 1) return true;
+
+        return false;
+    }
+
+    public void Turn(char mirror)
+    {
+        direction = direction switch
+        {
+            EDirection.East => mirror switch
+            {
+                '/' => EDirection.North,
+                '\\' => EDirection.South,
+                _ => direction
+            },
+            EDirection.North => mirror switch
+            {
+                '/' => EDirection.East,
+                '\\' => EDirection.West,
+                _ => direction
+            },
+            EDirection.West => mirror switch
+            {
+                '/' => EDirection.South,
+                '\\' => EDirection.North,
+                _ => direction
+            },
+            EDirection.South => mirror switch
+            {
+                '/' => EDirection.West,
+                '\\' => EDirection.East,
+                _ => direction
+            },
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public (int x, int y) Position { get => position; set => position = value; }
+
+    public EDirection Direction { get => direction; set => direction = value; }
+
+    public enum EDirection
+    {
+        North,
+        East,
+        South,
+        West
+    }
+}
---
+++ AocWebApp/Solvers/2023/Day16/Solver.cs
@@ -0,0 +1,130 @@
+﻿using Common;
+using Common.Solver;
+using Serilog;
+
+namespace Day16;
+
+[Day(2023, 16, "The Floor Will Be Lava")]
+internal class Solver : SolverBase
+{
+    private readonly string[] cave;
+
+    public Solver(string input, ILogger logger) : base(input, logger)
+    {
+        logger.Information("Scanning cave...");
+        cave = inputLines.Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+    }
+
+    public override Task Solve1()
+    {
+        logger.Information("Tracing rays...");
+        var count = Trace(new Ray((-1, 0), Ray.EDirection.East));
+
+        logger.Information("Finally there are {count} energized tiles", count);
+
+        return Task.CompletedTask;
+    }
+
+    public override Task Solve2()
+    {
+        logger.Information("Finding max number of energized tiles...");
+
+        var max = int.MinValue;
+
+        // Top 
+        for (var x = 0; x < cave[0].Length; x++)
+        {
+            var count = Trace(new Ray((x, -1), Ray.EDirection.South));
+            if (count > max) max = count;
+        }
+
+        // Bottom 
+        for (var x = 0; x < cave[0].Length; x++)
+        {
+            var count = Trace(new Ray((x, cave.Length), Ray.EDirection.North));
+            if (count > max) max = count;
+        }
+
+        // Left
+        for (var y = 0; y < cave.Length; y++)
+        {
+            var count = Trace(new Ray((-1, y), Ray.EDirection.East));
+            if (count > max) max = count;
+        }
+
+        // Right
+        for (var y = 0; y < cave.Length; y++)
+        {
+            var count = Trace(new Ray((cave[0].Length, y), Ray.EDirection.West));
+            if (count > max) max = count;
+        }
+
+        logger.Information("Max count {count} of energized tiles is", max);
+
+        return Task.CompletedTask;
+    }
+
+    private int Trace(Ray start)
+    {
+        var rays = new List<Ray> { start };
+
+        var energizedTiles = new HashSet<(int x, int y)>();
+        var alreadyTraced = new HashSet<((int x, int y), Ray.EDirection)>();
+
+        while (rays.Any())
+        {
+            var raysToRemove = new List<Ray>();
+            var raysToAdd = new List<Ray>();
+            foreach (var ray in rays)
+            {
+                ray.Move();
+                if (ray.IsOutOfBounds(cave))
+                {
+                    raysToRemove.Add(ray);
+                    continue;
+                }
+
+                if (alreadyTraced.Contains((ray.Position, ray.Direction)))
+                {
+                    raysToRemove.Add(ray);
+                    continue;
+                }
+
+                energizedTiles.Add(ray.Position);
+                alreadyTraced.Add((ray.Position, ray.Direction));
+
+                if (cave[ray.Position.y][ray.Position.x] == '-' && (ray.Direction is Ray.EDirection.North or Ray.EDirection.South))
+                {
+                    ray.Direction = Ray.EDirection.East;
+                    raysToAdd.Add(new Ray(ray.Position, Ray.EDirection.West));
+                }
+
+                if (cave[ray.Position.y][ray.Position.x] == '/' || cave[ray.Position.y][ray.Position.x] == '\\')
+                {
+                    ray.Turn(cave[ray.Position.y][ray.Position.x]);
+                }
+
+                if (cave[ray.Position.y][ray.Position.x] == '-' && (ray.Direction is Ray.EDirection.North or Ray.EDirection.South))
+                {
+                    ray.Direction = Ray.EDirection.East;
+                    raysToAdd.Add(new Ray(ray.Position, Ray.EDirection.West));
+                }
+
+                if (cave[ray.Position.y][ray.Position.x] == '|' && (ray.Direction is Ray.EDirection.East or Ray.EDirection.West))
+                {
+                    ray.Direction = Ray.EDirection.North;
+                    raysToAdd.Add(new Ray(ray.Position, Ray.EDirection.South));
+                }
+            }
+
+            foreach (Ray ray in raysToRemove)
+            {
+                rays.Remove(ray);
+            }
+
+            rays.AddRange(raysToAdd);
+        }
+
+        return energizedTiles.Count;
+    }
+}

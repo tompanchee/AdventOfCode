4619193060cd1a7bf7239737fdf3c4c65dc01f8b
Tom Pruiksma <tomppa.pruiksma@gmail.com>
2024-11-23 22:26:18 +0200

    2024 Day1 - Stub

---
+++ AocWebApp/Solvers/2024/Y24-Day01/Solver.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using Common.Solver;
+using Serilog;
+
+namespace Y24_Day01;
+
+[Day(2024, 1, "TODO:")]
+public class Solver : SolverBase
+{
+    public Solver(string input, ILogger logger) : base(input, logger)
+    {
+    }
+
+    public override Task Solve1()
+    {
+        return Task.CompletedTask;
+    }
+
+    public override Task Solve2()
+    {
+        return Task.CompletedTask;
+    }
+}
